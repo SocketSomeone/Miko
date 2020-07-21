3e520acbd7ace6f5a31e8946b27e8ac5073b9f8e
SocketSomeone <sleepth228@gmail.com>
2020-07-21 02:09:36 +0200

    Added Scripts

--- bin/Misc/Enums/Commands.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.standartCommands = exports.CommandGroup = void 0;
-var CommandGroup;
-(function (CommandGroup) {})((CommandGroup = exports.CommandGroup || (exports.CommandGroup = {})));
-var standartCommands;
-(function (standartCommands) {})((standartCommands = exports.standartCommands || (exports.standartCommands = {})));
-//# sourceMappingURL=Commands.js.map
--- bin/Misc/Models/Permissons.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=Permissons.js.map

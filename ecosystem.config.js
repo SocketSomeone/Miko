94721f87e9a7daaf28c73d50740ddb0e4e22d911
SocketSomeone <sleepth228@gmail.com>
2020-08-23 12:26:53 +0200

    .gitignore fix

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const { prod, maxShard, instance } = require('./config.json');
+
+const config = {
+	script: 'bin/root.js',
+	watch_delay: 500,
+	watch_options: {
+		followSymlinks: false
+	},
+	cron_restart: '1 0 * * *',
+	watch: ['node_modules', 'bin'],
+	log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS'
+};
+
+const apps = [];
+
+for (let j = 1; j <= maxShard; j++) {
+	apps.push({
+		name: `${instance} | Shard ${j}`,
+		args: `production ${prod.TOKEN} ${j} ${maxShard} --no-rabbitmq`,
+		...config
+	});
+}
+
+module.exports = {
+	apps
+};

7de348e890df87aa755af6283d94494c33f7a4da
SocketSomeone <sleepth228@gmail.com>
2020-09-12 21:34:50 +0200

    Fixed clearing cache while adding items to shop

--- src/Modules/Configure/Commands/removeshop.ts
+++ src/Modules/Configure/Commands/removeshop.ts
@@ -1,48 +1,60 @@
-import { Command, Context } from '../../../Framework/Commands/Command';
-import { BaseClient } from '../../../Client';
-import { NumberResolver } from '../../../Framework/Resolvers';
-import { CommandGroup } from '../../../Misc/Models/CommandGroup';
-import { Message } from 'eris';
-import { GuildPermission } from '../../../Misc/Models/GuildPermissions';
-import { BaseShopRole } from '../../../Entity/ShopRole';
-import { ExecuteError } from '../../../Framework/Errors/ExecuteError';
-import { Color } from '../../../Misc/Enums/Colors';
+import { Command, Context } from "../../../Framework/Commands/Command";
+import { BaseClient } from "../../../Client";
+import { NumberResolver } from "../../../Framework/Resolvers";
+import { CommandGroup } from "../../../Misc/Models/CommandGroup";
+import { Message } from "eris";
+import { GuildPermission } from "../../../Misc/Models/GuildPermissions";
+import { BaseShopRole } from "../../../Entity/ShopRole";
+import { ExecuteError } from "../../../Framework/Errors/ExecuteError";
+import { Color } from "../../../Misc/Enums/Colors";
 
 export default class extends Command {
-	public constructor(client: BaseClient) {
-		super(client, {
-			name: 'removeshop',
-			aliases: [],
-			args: [
-				{
-					name: 'index',
-					resolver: NumberResolver,
-					required: true
-				}
-			],
-			group: CommandGroup.CONFIGURE,
-			guildOnly: true,
-			premiumOnly: false,
-			botPermissions: [GuildPermission.MANAGE_ROLES],
-			userPermissions: [GuildPermission.MANAGE_GUILD, GuildPermission.MANAGE_ROLES],
-			examples: ['1']
-		});
-	}
+  public constructor(client: BaseClient) {
+    super(client, {
+      name: "removeshop",
+      aliases: [],
+      args: [
+        {
+          name: "index",
+          resolver: NumberResolver,
+          required: true,
+        },
+      ],
+      group: CommandGroup.CONFIGURE,
+      guildOnly: true,
+      premiumOnly: false,
+      botPermissions: [GuildPermission.MANAGE_ROLES],
+      userPermissions: [
+        GuildPermission.MANAGE_GUILD,
+        GuildPermission.MANAGE_ROLES,
+      ],
+      examples: ["1"],
+    });
+  }
 
-	public async execute(message: Message, [index]: [number], { funcs: { t, e }, guild, settings }: Context) {
-		const roles = await this.client.cache.shop.get(guild);
-		const role = roles[index - 1];
+  public async execute(
+    message: Message,
+    [index]: [number],
+    { funcs: { t, e }, guild, settings }: Context
+  ) {
+    const roles = await this.client.cache.shop.get(guild);
+    const role = roles[index - 1];
 
-		if (roles.length < 1 || !role) throw new ExecuteError(t('configure.removeshop.notFound'));
+    if (roles.length < 1 || !role)
+      throw new ExecuteError(t("configure.removeshop.notFound"));
 
-		role.remove().catch(() => undefined);
+    role.remove().catch(() => undefined);
 
-		await this.replyAsync(message, {
-			color: Color.MAGENTA,
-			title: t('configure.title'),
-			description: t('configure.removeshop.deleted', { role: `<@&${role.id}>` }),
-			footer: null,
-			timestamp: null
-		});
-	}
+    this.client.cache.shop.flush(guild.id);
+
+    await this.replyAsync(message, {
+      color: Color.MAGENTA,
+      title: t("configure.title"),
+      description: t("configure.removeshop.deleted", {
+        role: `<@&${role.id}>`,
+      }),
+      footer: null,
+      timestamp: null,
+    });
+  }
 }

476eb42ea27fa4e6d62f9dcf597862effd566ed6
SocketSomeone <sleepth228@gmail.com>
2020-09-13 01:41:37 +0200

    Fix pagination ID

--- src/Modules/Economy/Commands/shop.ts
+++ src/Modules/Economy/Commands/shop.ts
@@ -1,72 +1,85 @@
-import { Command, Context } from '../../../Framework/Commands/Command';
-import { BaseClient } from '../../../Client';
-import { CommandGroup } from '../../../Misc/Models/CommandGroup';
-import { Message } from 'eris';
-import { ExecuteError } from '../../../Framework/Errors/ExecuteError';
-import { NumberResolver } from '../../../Framework/Resolvers';
-import { Images } from '../../../Misc/Enums/Images';
+import { Command, Context } from "../../../Framework/Commands/Command";
+import { BaseClient } from "../../../Client";
+import { CommandGroup } from "../../../Misc/Models/CommandGroup";
+import { Message } from "eris";
+import { ExecuteError } from "../../../Framework/Errors/ExecuteError";
+import { NumberResolver } from "../../../Framework/Resolvers";
+import { Images } from "../../../Misc/Enums/Images";
 
 const ROLE_PER_PAGE = 8;
 
 export default class extends Command {
-	public constructor(client: BaseClient) {
-		super(client, {
-			name: 'shop',
-			aliases: ['магазин', 'шоп', 'магаз', 'items'],
-			args: [
-				{
-					name: 'page',
-					resolver: NumberResolver,
-					required: false
-				}
-			],
-			group: CommandGroup.ECONOMY,
-			guildOnly: true,
-			premiumOnly: false
-		});
-	}
+  public constructor(client: BaseClient) {
+    super(client, {
+      name: "shop",
+      aliases: ["магазин", "шоп", "магаз", "items"],
+      args: [
+        {
+          name: "page",
+          resolver: NumberResolver,
+          required: false,
+        },
+      ],
+      group: CommandGroup.ECONOMY,
+      guildOnly: true,
+      premiumOnly: false,
+    });
+  }
 
-	public async execute(message: Message, [offset]: [number], { funcs: { t, e }, guild, settings }: Context) {
-		const items = await this.client.cache.shop.get(guild);
+  public async execute(
+    message: Message,
+    [offset]: [number],
+    { funcs: { t, e }, guild, settings }: Context
+  ) {
+    const items = await this.client.cache.shop.get(guild);
 
-		if (items.length < 1) {
-			throw new ExecuteError(t('economy.shop.empty'));
-		}
+    if (items.length < 1) {
+      throw new ExecuteError(t("economy.shop.empty"));
+    }
 
-		const maxPage = Math.ceil(items.length / ROLE_PER_PAGE);
-		const startPage = Math.max(Math.min(offset ? offset - 1 : 0, maxPage - 1), 0);
+    const maxPage = Math.ceil(items.length / ROLE_PER_PAGE);
+    const startPage = Math.max(
+      Math.min(offset ? offset - 1 : 0, maxPage - 1),
+      0
+    );
 
-		await this.showPaginated(message, startPage, maxPage, (page) => {
-			const roles = items.slice(page * ROLE_PER_PAGE, (page + 1) * ROLE_PER_PAGE);
-			const fields: { name: string; value: string; inline: boolean }[] = [];
+    await this.showPaginated(message, startPage, maxPage, (page) => {
+      const roles = items.slice(
+        page * ROLE_PER_PAGE,
+        (page + 1) * ROLE_PER_PAGE
+      );
+      const fields: { name: string; value: string; inline: boolean }[] = [];
 
-			roles.map((role, i) => {
-				fields.push(
-					{
-						name: t('economy.shop.fields.index'),
-						value: `${i + 1}.`,
-						inline: true
-					},
-					{
-						name: t('economy.shop.fields.role'),
-						value: `<@&${role.id}>`,
-						inline: true
-					},
-					{
-						name: t('economy.shop.fields.cost'),
-						value: message.member.roles.includes(role.id)
-							? t('economy.shop.bought')
-							: `${role.cost} ${e(settings.currency)}`,
-						inline: true
-					}
-				);
-			});
+      roles.map((role, i) => {
+        fields.push(
+          {
+            name: t("economy.shop.fields.index"),
+            value: `${i + (page - 1) * ROLE_PER_PAGE + 1}.`,
+            inline: true,
+          },
+          {
+            name: t("economy.shop.fields.role"),
+            value: `<@&${role.id}>`,
+            inline: true,
+          },
+          {
+            name: t("economy.shop.fields.cost"),
+            value: message.member.roles.includes(role.id)
+              ? t("economy.shop.bought")
+              : `${role.cost} ${e(settings.currency)}`,
+            inline: true,
+          }
+        );
+      });
 
-			return this.createEmbed({
-				author: { name: t('economy.shop.title', { guild: guild.name }), icon_url: Images.SHOP },
-				fields,
-				footer: null
-			});
-		});
-	}
+      return this.createEmbed({
+        author: {
+          name: t("economy.shop.title", { guild: guild.name }),
+          icon_url: Images.SHOP,
+        },
+        fields,
+        footer: null,
+      });
+    });
+  }
 }
--- src/Modules/Permissions/Commands/list.ts
+++ src/Modules/Permissions/Commands/list.ts
@@ -1,44 +1,55 @@
-import { BaseClient } from '../../../Client';
-import { Context, Command } from '../../../Framework/Commands/Command';
-import { Message } from 'eris';
-import { CommandGroup } from '../../../Misc/Models/CommandGroup';
-import { GuildPermission } from '../../../Misc/Models/GuildPermissions';
-import { ExecuteError } from '../../../Framework/Errors/ExecuteError';
-import PermissionsOutput from '../Misc/PermissionsOutput';
-import { Color } from '../../../Misc/Enums/Colors';
-import { Images } from '../../../Misc/Enums/Images';
+import { BaseClient } from "../../../Client";
+import { Context, Command } from "../../../Framework/Commands/Command";
+import { Message } from "eris";
+import { CommandGroup } from "../../../Misc/Models/CommandGroup";
+import { GuildPermission } from "../../../Misc/Models/GuildPermissions";
+import { ExecuteError } from "../../../Framework/Errors/ExecuteError";
+import PermissionsOutput from "../Misc/PermissionsOutput";
+import { Color } from "../../../Misc/Enums/Colors";
+import { Images } from "../../../Misc/Enums/Images";
 
 const PERMS_PER_PAGE = 10;
 
 export default class extends Command {
-	public constructor(client: BaseClient) {
-		super(client, {
-			name: 'permissions list',
-			aliases: ['список правил'],
-			group: CommandGroup.PERMISSIONS,
-			args: [],
-			guildOnly: true,
-			premiumOnly: false,
-			userPermissions: [GuildPermission.ADMINISTRATOR]
-		});
-	}
+  public constructor(client: BaseClient) {
+    super(client, {
+      name: "permissions list",
+      aliases: ["список правил"],
+      group: CommandGroup.PERMISSIONS,
+      args: [],
+      guildOnly: true,
+      premiumOnly: false,
+      userPermissions: [GuildPermission.ADMINISTRATOR],
+    });
+  }
 
-	public async execute(message: Message, [n]: [number], { funcs: { t }, guild, settings }: Context) {
-		const permissions = await this.client.cache.permissions.get(guild);
+  public async execute(
+    message: Message,
+    [n]: [number],
+    { funcs: { t }, guild, settings }: Context
+  ) {
+    const permissions = await this.client.cache.permissions.get(guild);
 
-		if (permissions.length < 1) throw new ExecuteError(t('perms.cleared'));
+    if (permissions.length < 1) throw new ExecuteError(t("perms.cleared"));
 
-		const maxPage = Math.ceil(permissions.length / PERMS_PER_PAGE);
-		const startPage = n || 0;
+    const maxPage = Math.ceil(permissions.length / PERMS_PER_PAGE);
+    const startPage = n || 0;
 
-		await this.showPaginated(message, startPage, maxPage, (page) => {
-			const perms = permissions.slice(page * PERMS_PER_PAGE, (page + 1) * PERMS_PER_PAGE);
+    await this.showPaginated(message, startPage, maxPage, (page) => {
+      const perms = permissions.slice(
+        page * PERMS_PER_PAGE,
+        (page + 1) * PERMS_PER_PAGE
+      );
 
-			return this.createEmbed({
-				color: Color.MAGENTA,
-				author: { name: t('perms.title'), icon_url: Images.LIST },
-				description: perms.map((v, i) => PermissionsOutput(t, v, i)).join('\n')
-			});
-		});
-	}
+      return this.createEmbed({
+        color: Color.MAGENTA,
+        author: { name: t("perms.title"), icon_url: Images.LIST },
+        description: perms
+          .map((v, i) =>
+            PermissionsOutput(t, v, i + (page - 1) * PERMS_PER_PAGE)
+          )
+          .join("\n"),
+      });
+    });
+  }
 }

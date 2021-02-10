4dfed1b108f60d3de27c192c19824bbdeff8f254
SocketSomeone <sleepth228@gmail.com>
2021-02-10 14:15:46 +0200

    chore(eslint): change eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+/* eslint-disable */ 
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		"airbnb-base",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+		"plugin:@typescript-eslint/recommended"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: './tsconfig.eslint.json',
+		sourceType: "module",
+        tsconfigRootDir: __dirname
+	},
+	plugins: [
+		"import",
+		"@typescript-eslint"
+	],
+	rules: {
+		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				"selector": "interface",
+				"format": ["PascalCase"],
+				"prefix": ["I"]
+			}
+		],
+		"@typescript-eslint/ban-ts-comment": ["error", {
+			"ts-ignore": false
+		}],
+		"@typescript-eslint/indent": ["error"],
+		"@typescript-eslint/semi": ["error"],
+		"import/extensions": ["error", "ignorePackages", {
+			"js": "never",
+			"ts": "never"
+		}],
+		"import/prefer-default-export": "off",
+        "import/no-default-export": ["error"],
+        "import/no-cycle": "off",
+		"no-restricted-syntax": ["error", "WithStatement"],
+		"linebreak-style": ["error", "unix"],
+		"comma-dangle": ["error", "never"],
+		"no-param-reassign": ["error", {
+			"props": false
+		}],
+		"no-dupe-class-members": "off",
+		"no-await-in-loop": "off",
+		"arrow-parens": "off",
+		"no-continue": "off",
+		"no-tabs": ["error", {
+			"allowIndentationTabs": true
+        }],
+		"indent": "off",
+        "max-len": ["error", { "code": 150 }]
+	},
+	overrides: [
+		{
+			files: ["{apps,packages,tools}/*/test/**/*.ts"],
+			env: {
+				jest: true
+			}
+		}
+	]
+}

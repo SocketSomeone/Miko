943980ca2b6375290b873865291c1723c1d0472e
SocketSomeone <sleepth228@gmail.com>
2021-02-21 00:48:18 +0200

    Deleted dummy test

--- packages/common/test/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { container } from 'tsyringe';
-import { MiClient } from '../src/client';
-
-test('Resolve Client in Container', () => {
-	expect(container.resolve(MiClient)).not.toBeNull();
-});
